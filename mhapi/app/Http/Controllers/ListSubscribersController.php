5a2c4af3644237a1b5c0f397399828bc8501681a
smallfri <russell@smallfri.com>
2016-02-04 09:02:23 -0500

    ADDED error responses to listsubscriberscontroller

--- mhapi/app/Http/Controllers/ListSubscribersController.php
+++ mhapi/app/Http/Controllers/ListSubscribersController.php
@@ -30,20 +30,40 @@ class ListSubscribersController extends ApiController
 
         $data = json_decode(file_get_contents('php://input'), true);
 
+        if(empty($data))
+        {
+            return $this->respondWithError('No data found, please check your POST data and try again');
+        }
+
+        $expected_input = [
+            'list_uid',
+            'email',
+            'firstname',
+            'lastname'
+        ];
+
+        $missing_fields = array();
+
+        foreach($expected_input AS $input)
+        {
+            if(!isset($data[$input]))
+            {
+                $missing_fields[$input] = 'Input field not found.';
+            }
+
+        }
+
+        if(!empty($missing_fields))
+        {
+            return $this->respondWithError($missing_fields);
+        }
+
         $response = $this->endpoint->create($data['list_uid'], array(
             'EMAIL' => isset($data['email'])?$data['email']:null,
             'FNAME' => isset($data['firstname'])?$data['firstname']:null,
             'LNAME' => isset($data['lastname'])?$data['lastname']:null,
         ));
         $response = $response->body;
-
-//        print_r($response);exit;
-
-//        if($response->body['status']=='error')
-//        {
-//            $msg = $response->body['error']['general'];
-//            return $this->respondWithError($msg);
-//        }
 
         return $this->respond(['message' => 'subscriber added.']);
     }
@@ -53,6 +73,31 @@ class ListSubscribersController extends ApiController
 
         $data = json_decode(file_get_contents('php://input'), true);
 
+        if(empty($data))
+        {
+            return $this->respondWithError('No data found, please check your POST data and try again');
+        }
+
+        $expected_input = [
+            'list_uid',
+            'email'
+        ];
+
+        $missing_fields = array();
+
+        foreach($expected_input AS $input)
+        {
+            if(!isset($data[$input]))
+            {
+                $missing_fields[$input] = 'Input field not found.';
+            }
+
+        }
+
+        if(!empty($missing_fields))
+        {
+            return $this->respondWithError($missing_fields);
+        }
 
         // UNSUBSCRIBE existing subscriber, no email is sent, unsubscribe is silent
         $response = $this->endpoint->unsubscribeByEmail($data['list_uid'], $data['email']);
@@ -70,7 +115,7 @@ class ListSubscribersController extends ApiController
     {
 
         // SEARCH BY EMAIL
-       $ListSubscriber = ListsSubscriber::where('email','=',$email)->get();
+        $ListSubscriber = ListsSubscriber::where('email', '=', $email)->get();
 
         if(empty($ListSubscriber[0]))
         {
@@ -79,7 +124,5 @@ class ListSubscribersController extends ApiController
 
         return $this->respond(['subscriber_uid' => $ListSubscriber[0]->subscriber_uid]);
     }
-
-
 
 }
