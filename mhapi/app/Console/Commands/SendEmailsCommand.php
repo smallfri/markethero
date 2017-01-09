364865f956f0f714ee665afa51f0e9e1a9db80f3
smallfri <russell@smallfri.com>
2017-01-09 12:55:00 -0500

    CLEANED up code

--- mhapi/app/Console/Commands/SendEmailsCommand.php
+++ mhapi/app/Console/Commands/SendEmailsCommand.php
@@ -430,17 +430,7 @@ class SendEmailsCommand extends Command
 
         $this->stdout('Limit '.$limit.' / '.'Offset '.$offset);
 
-
-        $emailsCount = count($emails);
-
         $this->stdout(sprintf('Found %s emails for this batch', count($emails)));
-
-//        if ($emailsCount<=$offset||$emailsCount<$offset)
-//        {
-//            return;
-//        }
-
-//        $this->updateGroupStatus($group->group_email_id, GroupEmailGroupsModel::STATUS_PROCESSING);
 
         $this->stdout(sprintf("This emails worker(#%d) will process %d emails for this group...", $workerNumber,
             count($emails)));
@@ -452,17 +442,17 @@ class SendEmailsCommand extends Command
 
             $this->stdout('Adding email '.$data['to_email']);
 
-            $job = (new SendEmail($data))->onConnection('mail-queue');
-            app('Illuminate\Contracts\Bus\Dispatcher')->dispatch($job);
-
-            $this->updateGroupEmailsToSent($data->email_id, GroupEmailGroupsModel::STATUS_QUEUED);
-
-
-//            $this->stdout('Sending email '.$data['to_email']);
+//            $job = (new SendEmail($data))->onConnection('mail-queue');
+//            app('Illuminate\Contracts\Bus\Dispatcher')->dispatch($job);
 //
-//            $this->sendByPHPMailer(json_decode(json_encode($data)));
-//
-//            $this->updateGroupEmailsToSent($data->email_id, GroupEmailGroupsModel::STATUS_SENT);
+//            $this->updateGroupEmailsToSent($data->email_id, GroupEmailGroupsModel::STATUS_QUEUED);
+
+
+            $this->stdout('Sending email '.$data['to_email']);
+
+            $this->sendByPHPMailer(json_decode(json_encode($data)));
+
+            $this->updateGroupEmailsToSent($data->email_id, GroupEmailGroupsModel::STATUS_SENT);
 
         }
 
@@ -486,138 +476,137 @@ class SendEmailsCommand extends Command
         return 0;
     }
 
-
     public function sendByPHPMailer($data)
+    {
+
+        $customer = Customer::find($data->customer_id);
+
+        $server = DeliveryServerModel::find($customer->group_pool_id);
+
+        if (empty($server))
+        {
+            $server = DeliveryServerModel::find(1);
+        }
+
+        if (isset($data->group_email_id)&&$data->group_email_id>1)
+        {
+            $group_email_id = $data->group_email_id;
+        }
+        else
+        {
+            $group_email_id = 1;
+        }
+
+        $pause = PauseGroupEmailModel::where('group_email_id', '=', $data->group_email_id)
+            ->orWhere('customer_id', '=', $data->customer_id)
+            ->get();
+
+        if (count($pause))
+        {
+            $pause = $pause[0];
+
+            if (!empty($pause))
+            {
+                if ($pause->group_email_id==$data->group_email_id||$pause->pause_customer==1)
+                {
+                    $this->delete();
+
+                    GroupEmailModel::where('email_uid', '=', $data->email_uid)
+                        ->update('status', '=', GroupEmailGroupsModel::STATUS_PAUSED);
+                    return false;
+                }
+            }
+        }
+
+        try
         {
 
-            $customer = Customer::find($data->customer_id);
+            $mail = New \PHPMailer();
+            $mail->SMTPKeepAlive = true;
 
-            $server = DeliveryServerModel::find($customer->group_pool_id);
+            $mail->isSMTP();
+            $mail->CharSet = "utf-8";
+            $mail->SMTPAuth = true;
+            $mail->SMTPSecure = "tls";
+            $mail->Host = $server->hostname;
+            $mail->Port = 2525;
+            $mail->Username = $server->username;
+            $mail->Password = base64_decode($server->password);
+            $mail->Sender = Helpers::findBounceServerSenderEmail($server->bounce_server_id);
 
-            if (empty($server))
+            $mail->addCustomHeader('X-Mw-Customer-Id', $data->customer_id);
+            $mail->addCustomHeader('X-Mw-Email-Uid', $data->email_uid);
+            $mail->addCustomHeader('X-Mw-Group-Id', $group_email_id);
+            if ($group_email_id==1)
             {
-                $server = DeliveryServerModel::find(1);
+                $mail->addCustomHeader('X-Mw-Transactional-Id', $group_email_id);
             }
 
-            if (isset($data->group_email_id)&&$data->group_email_id>1)
+            $mail->addReplyTo($data->from_email, $data->from_name);
+            $mail->setFrom($data->from_email, $data->from_name);
+            $mail->addAddress($data->to_email, $data->to_name);
+
+            $mail->Subject = $data->subject;
+            $mail->MsgHTML($data->body);
+
+            if (!$mail->send())
             {
-                $group_email_id = $data->group_email_id;
+                // save status failed if mail did not send
+                $status = 'failed';
             }
             else
             {
-                $group_email_id = 1;
+                // save status sent if mail DID send
+                $status = 'sent';
             }
 
-            $pause = PauseGroupEmailModel::where('group_email_id', '=', $data->group_email_id)
-                ->orWhere('customer_id', '=', $data->customer_id)
-                ->get();
+            $mail->clearAddresses();
+            $mail->clearAttachments();
+            $mail->clearCustomHeaders();
 
-            if (count($pause))
-            {
-                $pause = $pause[0];
+        } catch (\Exception $e)
+        {
+            print_r($e);
+            // save status error if try/catch returns error
+            $status = 'error';
 
-                if (!empty($pause))
-                {
-                    if ($pause->group_email_id==$data->group_email_id||$pause->pause_customer==1)
-                    {
-                        $this->delete();
-
-                        GroupEmailModel::where('email_uid', '=', $data->email_uid)
-                            ->update('status', '=', GroupEmailGroupsModel::STATUS_PAUSED);
-                        return false;
-                    }
-                }
-            }
-
-            try
-            {
-
-                $mail = New \PHPMailer();
-                $mail->SMTPKeepAlive = true;
-
-                $mail->isSMTP();
-                $mail->CharSet = "utf-8";
-                $mail->SMTPAuth = true;
-                $mail->SMTPSecure = "tls";
-                $mail->Host = $server->hostname;
-                $mail->Port = 2525;
-                $mail->Username = $server->username;
-                $mail->Password = base64_decode($server->password);
-                $mail->Sender = Helpers::findBounceServerSenderEmail($server->bounce_server_id);
-
-                $mail->addCustomHeader('X-Mw-Customer-Id', $data->customer_id);
-                $mail->addCustomHeader('X-Mw-Email-Uid', $data->email_uid);
-                $mail->addCustomHeader('X-Mw-Group-Id', $group_email_id);
-                if ($group_email_id==1)
-                {
-                    $mail->addCustomHeader('X-Mw-Transactional-Id', $group_email_id);
-                }
-
-                $mail->addReplyTo($data->from_email, $data->from_name);
-                $mail->setFrom($data->from_email, $data->from_name);
-                $mail->addAddress($data->to_email, $data->to_name);
-
-                $mail->Subject = $data->subject;
-                $mail->MsgHTML($data->body);
-
-                if (!$mail->send())
-                {
-                    // save status failed if mail did not send
-                    $status = 'failed';
-                }
-                else
-                {
-                    // save status sent if mail DID send
-                    $status = 'sent';
-                }
-
-                $mail->clearAddresses();
-                $mail->clearAttachments();
-                $mail->clearCustomHeaders();
-
-            } catch (\Exception $e)
-            {
-                print_r($e);
-                // save status error if try/catch returns error
-                $status = 'error';
-
-            }
+        }
 //            $this->delete();
 
         print_r($data);
 
-            $this->replyToMarketHero($data);
+        $this->replyToMarketHero($data);
 
-            $update = GroupEmailModel::find($data->email_id);
-            $update->status = $status;
-            $update->last_updated = new \DateTime();
-            $update->save();
+        $update = GroupEmailModel::find($data->email_id);
+        $update->status = $status;
+        $update->last_updated = new \DateTime();
+        $update->save();
 
-        }
+    }
 
-        public function replyToMarketHero($Email)
-        {
+    public function replyToMarketHero($Email)
+    {
 
-            $conf = new Conf();
-            $conf->set('security.protocol', 'plaintext');
-            $conf->set('broker.version.fallback', '0.8.2.1');
+        $conf = new Conf();
+        $conf->set('security.protocol', 'plaintext');
+        $conf->set('broker.version.fallback', '0.8.2.1');
 
-            $rk = new Producer($conf);
-            $rk->setLogLevel(LOG_DEBUG);
-            $rk->addBrokers("kafka-3.int.markethero.io, kafka-2.int.markethero.io,kafka-1.int.markethero.io");
+        $rk = new Producer($conf);
+        $rk->setLogLevel(LOG_DEBUG);
+        $rk->addBrokers("kafka-3.int.markethero.io, kafka-2.int.markethero.io,kafka-1.int.markethero.io");
 
-            $topic = $rk->newTopic("email_one_email_sent");
-            $date = date_create();
+        $topic = $rk->newTopic("email_one_email_sent");
+        $date = date_create();
 
-            $message = [
-                'mhEmailID' => $Email->mhEmailID,
-                'emailOneEmailID' => $Email->email_uid,
-                'sentDateTime' => date_format($date, 'U')
-            ];
+        $message = [
+            'mhEmailID' => $Email->mhEmailID,
+            'emailOneEmailID' => $Email->email_uid,
+            'sentDateTime' => date_format($date, 'U')
+        ];
 
-            $topic->produce(RD_KAFKA_PARTITION_UA, 0, json_encode($message));
-            //        var_dump(json_encode($message));
-        }
+        $topic->produce(RD_KAFKA_PARTITION_UA, 0, json_encode($message));
+        //        var_dump(json_encode($message));
+    }
 
 
 
